c1e848e2d9b4b599e3f0d389f7ce2bcdf586fbe2
Fedor Riabov <fedorrik1@gmail.com>
2024-09-10 12:25:43 -0700

    implement .bam support

--- count_kmers_in_reads.sh
+++ count_kmers_in_reads.sh
@@ -10,23 +10,36 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2:00:00
 
-fastq_path=$1
+reads_path=$1
 kmers_tsv=$2
 
-fastq=`echo $fastq_path | awk '{n=split($0, name, "/"); print name[n]}'`
+fastq=`echo $reads_path | awk '{n=split($0, name, "/"); print name[n]}'`
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 echo $fastq
 
-for fv in `cut -f 2 $kmers_tsv`
-do
+for fv in `cut -f 2 $kmers_tsv`; do
+
   rc=`echo $fv | tr ACGTacgt TGCAtgca | rev`
   echo -n "$fv $rc "; date +"%D %T"
 
-  if (file $fastq_path | grep -q compressed )
-  then
-    zgrep -oP "$fv|$rc" $fastq_path | wc -l >> "$fastq.cnt"
+  # fastq.gz
+  if (echo $reads_path | grep -q ".fastq.gz" ); then
+    zgrep -oP "$fv|$rc" $reads_path | wc -l >> "$fastq.cnt"
+
+  # fastq
+  elif (echo $reads_path | grep -q ".fastq" ); then
+    grep -oP "$fv|$rc" $reads_path | wc -l >> "$fastq.cnt"
+
+  # bam
+  elif (echo $reads_path | grep -q ".bam" ); then
+    samtools view $reads_path | grep -oP "$fv|$rc" | wc -l >> "$fastq.cnt"
+  
+  # wrng file
   else
-    grep -oP "$fv|$rc" $fastq_path | wc -l >> "$fastq.cnt"
+    echo "Wrong input file format (.fastq/.fastq.gz/.bam required)"
+    echo "Usage: ./count_kmers_in_reads.sh kmer.tsv reads" 
+
   fi
+
 done
